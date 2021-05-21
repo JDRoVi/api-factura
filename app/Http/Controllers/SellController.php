c4c215cc39775d38cd5651705330dc23c7925587
Jose Daniel Rodriguez Villalobos <68168528+JDRoVi@users.noreply.github.com>
2021-05-21 12:07:23 -0600

    Ajuste de SellController

--- app/Http/Controllers/SellController.php
+++ app/Http/Controllers/SellController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Sell;
 use Illuminate\Http\Request;
+use App\Models\SellDetails;
 
 class SellController extends Controller
 {
@@ -41,44 +42,36 @@ class SellController extends Controller
     {
         $json = $request->input('json', null);
         $data = json_decode($json, true);
-        if(!empty($data)) {
-            $data = array_map('trim', $data);
-            $rules = [
-                'id' => 'required|numeric',
-                'idCajero' => 'required|numeric',
-                'idDetalleVenta' => 'required|numeric',
-                'idCliente' => 'required|numeric',
-                'fecha' => 'required',
-                'total' => 'required|numeric'
-            ];
-            $valid = \validator($data, $rules);
-            if ($valid->fails()) {
-                $response = array(
-                    'status' => 'error',
-                    'code' => 406,
-                    'message' => 'Los datos son incorrectos',
-                    'errors' => $valid->errors()
-                );
-            } else {
-                $sells = Product::where('codigoProducto',$data['codigoProducto'])->first();
-                $sell = new Sell();
-                $sell -> idCajero = $data['idCajero'];
-                $sell -> idCliente = $data['idCliente'];
-                $sell -> idDetalleVenta = $data['idDetalleVenta'];
-                $sell -> fecha = $data['fecha'];
-                $sell -> total = $sells['total'];
-                $sell -> save();
-                $response = array(
-                    'status' => 'success',
-                    'code' => 200,
-                    'message' => 'Datos almacenados satisfactoriamente'
-                );
-            }
-        } else {
+        $data = array_map('trim', $data);
+        $rules = [
+            'id' => 'required|numeric',
+            'idCajero' => 'required|numeric',
+            'idDetalleVenta' => 'required|numeric',
+            'idCliente' => 'required|numeric',
+            'fecha' => 'required',
+            'total' => 'required|numeric'
+        ];
+        $valid = \validator($data, $rules);
+        if ($valid->fails()) {
             $response = array(
-                'status' => 'NOT FOUND',
-                'code' => 404,
-                'message' => 'Datos no encontrados'
+                'status' => 'error',
+                'code' => 406,
+                'message' => 'Los datos son incorrectos',
+                'errors' => $valid->errors()
+            );
+        } else {
+            $sells = SellDetails::where('idVenta',$data['idDetalleVenta'])->first();
+            $sell = new Sell();
+            $sell -> idCajero = $data['idCajero'];
+            $sell -> idCliente = $data['idCliente'];
+            $sell -> idDetalleVenta = $data['idDetalleVenta'];
+            $sell -> fecha = $data['fecha'];
+            $sell -> total = $sells;
+            $sell -> save();
+            $response = array(
+                'status' => 'success',
+                'code' => 200,
+                'message' => 'Datos almacenados satisfactoriamente'
             );
         }
         return response()->json($response, $response['code']);
@@ -90,9 +83,9 @@ class SellController extends Controller
      * @param  \App\Models\Sell  $sell
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($idSell)
     {
-        $data = Sell::find($id);
+        $data = Sell::where('idDetalleVenta', $idSell)->first();
         if (is_object($data)) {
             $response = array(
                 'status' => 'success',
@@ -109,95 +102,4 @@ class SellController extends Controller
         return response()->json($response,$response['code']);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Sell  $sell
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request)
-    {
-        $json = $request->input('json', null);
-        $data = json_decode($json, true);
-        if (!empty($data)) {
-            $data = array_map('trim', $data);
-            $rules = [
-                'id' => 'required|numeric',
-                'idCajero' => 'required|numeric',
-                'idDetalleVenta' => 'required|numeric',
-                'idCliente' => 'required|numeric',
-                'fecha' => 'required',
-                'total' => 'required|numeric'
-            ];
-            $validate = \validator($data, $rules);
-            if ($validate->fails()) {
-                $response = array(
-                    'status' => 'error',
-                    'code' => 406,
-                    'message' => 'Los datos enviados son incorrectos',
-                    'errors' => $validate->errors()
-                );
-            } else {
-                $id=$data['id'];
-                unset($data['id']);
-                unset($data['created_at']);
-                $updated = Sell::where('id', $id)->update($data);
-                if ($updated > 0) {
-                    $response = array(
-                        'status' => 'success',
-                        'code' => 200,
-                        'message' => 'Datos actualizados exitosamente'
-                    );
-                } else {
-                    $response = array(
-                        'status' => 'error',
-                        'code' => 400,
-                        'message' => 'No se pudo actualizar los datos'
-                    );
-                }
-            }
-        } else {
-            $response = array(
-                'status' => 'error',
-                'code' => 400,
-                'message' => 'Faltan Datos'
-            );
-        }
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Sell  $sell
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        if (isset($id)) {
-            $deleted = Sell::where('id', $id)->delete();
-            if ($deleted) {
-                $response = array(
-                    'status' => 'success',
-                    'code' => 200,
-                    'message' => 'Eliminado correctamente'
-                );
-            } else {
-                $response = array(
-                    'status' => 'error',
-                    'code' => 400,
-                    'message' => 'Problemas al eleminar el recurso, puede ser que el recurso no exista'
-                );
-            }
-        } else {
-            $response = array(
-                'status' => 'error',
-                'code' => 400,
-                'message' => 'Falta el identificador del recurso'
-            );
-        }
-        return response()->json($response, $response['code']);
-    }
-
-    
 }
